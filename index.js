6f5c5f7d76b0f2f4c56d75d8de28757211ed994b
Ellen Keal <ellenkeal@gmail.com>
2017-08-18 22:55:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(array1, array2) {
+  var strings = [];
+  for (var i = 0; i < array1.length, i++) {
+    strings.push("${array1[i]} plays ${array2[2]}");
+  }
+  return strings;
+}
